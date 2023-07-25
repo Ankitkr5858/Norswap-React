87e64c4928091686ee87f51c7804ca9925ff43d8
Ankitkr5858 <ankitkr5858@gmail.com>
2023-07-25 14:10:56 +0500

    video updated

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -4,7 +4,7 @@ import ReadyForVideoBg from "../assets/video/ready_for_nordek.mp4";
 const Video = () => {
   return (
     <video
-      className="absolute top-0 left-0 z-[-1] min-w-full min-h-full"
+      className="absolute top-[-70px] left-0 z-[-1] min-w-full min-h-full"
       height="100%"
       width="100%"
       autoplay="autoplay"
